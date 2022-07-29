7ef84b04ed8f171f171c16b8bfba4a42ee3805b6
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-07-29 12:37:54 +0000

    initiating backend and server

---
+++ backend/api/movies.route.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const router = express.Router();
+router.route("/").get((req, res) => res.send("hello world"));
+export default router;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
-import app from "./server.js"
+import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
-async function main(){
-    dotenv.config()
-    const client = new mongodb.MongoClient()
+async function main() {
+  dotenv.config();
+  const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URL);
+
+  const port = process.env.PORT || 8000;
+  try {
+    await client.connect();
+    app.listen(port, () => {
+      console.log("server is running on port:" + port);
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 }
+
+main().catch(console.error);
