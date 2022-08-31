ffa4c088e706e328182e7952b7f0e735158c68d1
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-08-31 13:17:02 +0000

    Update movies.route.js

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,10 +1,13 @@
 import express from "express";
 import MoviesController from "./movies.controller.js";
+import ReviewsController from "./reviews.controller.js";
 
 const router = express.Router();
 router.route("/").get(MoviesController.apiGetMovies);
 
-router.route("/action").get((req, res) => {
-  res.send("Welcome to Action Movies");
-});
+router
+  .route("/review")
+  .post(ReviewsController.apiPostReview)
+  .put(ReviewsController.apiUpdateReview)
+  .delete(ReviewsController.apiDeleteReview);
 export default router;
