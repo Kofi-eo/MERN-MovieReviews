d357e12f5e23cec18806cb3b18bdd5c10f58e8d8
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-08-31 17:56:55 +0000

    Update movies.controller.js

--- backend/api/movies.controller.js
+++ backend/api/movies.controller.js
@@ -31,4 +31,32 @@ export default class MoviesController {
 
     res.json(response);
   }
+
+  static async apiGetMovieById(req, res, next) {
+    try {
+      let id = req.params.id || {};
+
+      let movie = await MoviesDAO.getMovieById(id);
+
+      if (!movie) {
+        res.status(404).json({ error: "not found" });
+        return;
+      }
+
+      res.json(movie);
+    } catch (e) {
+      console.log(`api, ${e}`);
+      res.status(500).json({ error: e });
+    }
+  }
+
+  static async apiGetRatings(req, res, next) {
+    try {
+      let propertyTypes = await MoviesDAO.getRatings();
+      res.json(propertyTypes);
+    } catch (e) {
+      console.log(`api,${e}`);
+      res.status(500).json({ error: e });
+    }
+  }
 }
