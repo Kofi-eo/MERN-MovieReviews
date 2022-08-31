8bfe4622090fc691d5c071991544088a92775bee
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-08-31 13:17:05 +0000

    Create reviews.controller.js

---
+++ backend/api/reviews.controller.js
@@ -0,0 +1,68 @@
+import ReviewsDAO from "../dataAccessObject/reviewsDAO.js";
+
+export default class ReviewsController {
+  static async apiPostReview(req, res, next) {
+    try {
+      const movieID = req.body.movie_id;
+      const review = req.body.review;
+      const userInfo = {
+        name: req.body.name,
+        _id: req.body.user_id,
+      };
+
+      const date = new Date();
+
+      const ReviewResponse = await ReviewsDAO.addReview(
+        movieID,
+        userInfo,
+        review,
+        date
+      );
+      res.json({ status: "success" });
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  }
+
+  static async apiUpdateReview(req, res, next) {
+    try {
+      const reviewId = req.body.review_id;
+      const review = req.body.review;
+
+      const date = new Date();
+
+      const ReviewResponse = await ReviewsDAO.updateReview(
+        reviewId,
+        req.body.user_id,
+        review,
+        date
+      );
+
+      var { error } = ReviewResponse;
+
+      if (error) {
+        res.status.json({ error });
+      }
+
+      if (ReviewResponse.modifiedCount === 0) {
+        throw new Error(
+          "unable to update review. User may not be original poster"
+        );
+      }
+      res.json({ status: "success" });
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  }
+
+  static async apiDeleteReview(req, res, next) {
+    try {
+      const reviewId = req.body.review_id;
+      const userId = req.body.user_id;
+      const ReviewResponse = await ReviewsDAO.deleteReview(reqId, userId);
+      res.json({ status: "success" });
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  }
+}
