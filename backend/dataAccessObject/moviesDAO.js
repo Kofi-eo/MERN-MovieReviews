1db9de13deeaf29b55ea6c4f85a37d7dff504bc4
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-08-31 17:57:04 +0000

    Update moviesDAO.js

--- backend/dataAccessObject/moviesDAO.js
+++ backend/dataAccessObject/moviesDAO.js
@@ -1,3 +1,5 @@
+import mongodb, { ObjectId } from "mongodb"
+
 let movies;
 
 export default class MoviesDAO {
@@ -44,6 +46,30 @@ export default class MoviesDAO {
       console.error(`Unable to issue find command, ${e}`);
 
       return { moviesList: [], totalNumMovies: 0 };
+    }
+  }
+
+  static async getRatings(){
+    let ratings = []
+    try{
+      ratings = await movies.distinct("rated")
+      return ratings
+    }
+    catch(e){
+      console.error(`unable to get ratings, ${e}`)
+      return ratings;
+    }
+  }
+
+  static async getMovieById(id){
+    try{
+return await movies.aggregate([
+  {$match:{_id:new ObjectId(id)}},{$lookup:{from:'reviews',localField:'_id',foreignField:'movie_id',
+as:'reviews'}}
+]).next()
+    }catch(e){
+console.error(`something went wrong in getMoviesById:${e}`)
+throw e
     }
   }
 }
