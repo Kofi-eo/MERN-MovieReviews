15f6d24284b1f4fbf28557b86f7da0dcf5297198
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-09-01 23:29:19 +0000

    Adding new components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
+import addReview from "./components/addReview";
+import movie from "./components/movie";
+import moviesList from "./components/moviesList";
+import login from "./components/login";
+import NavBar from "./components/navBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <NavBar />;
 }
 
 export default App;
