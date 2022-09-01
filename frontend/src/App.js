497e7a77ab1e785ff9136e076389e6e7591205fd
Emmanuel Ofori <103290939+Kofi-eo@users.noreply.github.com>
2022-09-01 16:07:08 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
